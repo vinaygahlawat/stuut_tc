b16fa82bfe46b2ead76f7e2ca957146ab1774b16
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-06-21 12:22:21 -0400

    Server changes to access yfinance module.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import yfinance as yf
 
 app = Flask(__name__)
 CORS(app)
@@ -9,5 +10,12 @@ def get_data():
     data = {"message": "Hello, World!"}
     return jsonify(data)
 
+@app.route('/api/ticker/<symbol>', methods=['GET'])
+def get_ticker_data(symbol):
+    ticker = yf.Ticker(symbol)
+    print(ticker.info)
+    return jsonify(ticker.info)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
